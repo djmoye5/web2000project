2d3732be6749345972675cbe6aca7738e00822c5
Denerius Moye <dmoye@techlaunch.io>
2019-04-02 19:36:23 -0400

    first commit

---
+++ project.js
@@ -0,0 +1,89 @@
+var albums = [
+    {
+        id: 1,
+        title: "Smooth R&B Reel",
+        author: "Vincent Augustus",
+        picture: "https://images.pexels.com/photos/1626481/pexels-photo-1626481.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        songs: [
+            {
+                pos: 1,
+                title: "We Go",
+                size: "5.2",
+                length: "2:16",
+                file: "music/Vincent_Augu_-_03_-_we_go.mp3",
+            },
+            {
+                pos: 2,
+                title: "Outline",
+                size: "6.4",
+                length: "2:48",
+                file: "music/Vincent_Augustus_-_01_-_outline.mp3"
+            },
+            {
+                pos: 3,
+                title: "She's Wrong",
+                size: "6.7",
+                length: "2:55",
+                file: "music/Vincent_Augustus_-_04_-_shes_wrong.mp3"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Rock Until the Morning",
+        author: "XTaKeRuX",
+        picture: "https://images.pexels.com/photos/6966/abstract-music-rock-bw.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        songs: [
+            {
+                pos: 1,
+                title: "Sail Away",
+                size: "2.9",
+                length: "1.16",
+                file: "music/XTaKeRuX_-_04_-_Sail_Away.mp3"
+            },
+            {
+                pos: 2,
+                title: "Rocking Forward",
+                size: "9.1",
+                length: "3:57",
+                file: "music/XTaKeRuX_-_01_-_Rocking_Forward.mp3"
+            },
+            {
+                pos: 3,
+                title: "Lost",
+                size: "3.1",
+                length: "1:21",
+                file: "music/XTaKeRuX_-_04_-_Lost.mp3"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Trap Beatz",
+        author: "Yung Kartz",
+        picture: "https://images.pexels.com/photos/257904/pexels-photo-257904.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        songs: [
+            {
+                pos: 1,
+                title: "Outcast",
+                size: "6.1",
+                length: "2:56",
+                file: "music/Yung_Kartz_-_15_-_OUTCAST.mp3",
+            },
+            {
+                pos: 2,
+                title: "Bankroll",
+                size: "6.9",
+                length: "3:23",
+                file: "music/Yung_Kartz_-_07_-_Bankroll.mp3",
+            },
+            {
+                pos: 3,
+                title: "Number One",
+                size: "7.5",
+                length: "3:44",
+                file: "music/Yung_Kartz_-_01_-_Number_One.mp3",
+            }
+        ]
+    },
+]
